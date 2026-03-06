d414130ed0d17a16bf3fa5e95a6d2cf5aea25e87
websample.steamspace <65386045+iyus123@users.noreply.github.com>
2026-03-06 21:21:31 +0700

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,5 @@
 import AdminDashboard from "@/components/AdminDashboard";
-import { env } from "@/lib/env";
 
 export default function AdminPage() {
-  return <AdminDashboard storeName={env.storeName} whatsappNumber={env.whatsappNumber} />;
+  return <AdminDashboard />;
 }
